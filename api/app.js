f3db093aa3d15f8f41bbe2216a20e61ff4ffa945
ThotaSridharRao <t.shridher@gmail.com>
2025-09-16 20:48:56 +0530

    fix: Correct static file serving path from 'src/public' to 'api/public'

--- api/app.js
+++ api/app.js
@@ -56,7 +56,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 // Middleware to serve static files from the 'public' directory
-app.use(express.static("src/public"));
+app.use(express.static("api/public"));
 
 // --- Route Imports ---
 import authRouter from "./routes/auth.routes.js";
